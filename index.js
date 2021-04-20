abdd159961dc739ff9c19616fe59a19543246102
Nickie Frausto <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-04-19 22:25:15 -0500

    added prompt.inquirer and first question

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const mysql=require('mysql');
-const connection=mysql.createConnection({ host: 'localhost', port: 3306, user: 'root', password: '', database '',});
+const mysql = require("mysql");
 
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "Emp_Tracker",
+});
+
+const start = () => {
+  inquirer.prompt([
+    {
+      name: "viewAddUpdateRemove",
+      type: "list",
+      message:
+        "Welcome to the Employee Tracker, please make a selection from below.",
+      choices: [
+        "View employees",
+        "Add a new employee",
+        "Update an employee record",
+        "Remove an employee",
+        "View the total budget per department",
+      ],
+    },
+    console.log("success!"),
+  ]);
+};
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log();
-    // readColleges();
-  });
-  
+  if (err) throw err;
+  console.log("connected");
+  start();
+});
